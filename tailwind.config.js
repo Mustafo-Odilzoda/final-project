156b3f0c7243a1f463016aedb4ab0e82459c13f4
Mustafo-Odilzoda <144124566+Mustafo-Odilzoda@users.noreply.github.com>
2023-10-28 17:19:06 +0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        myyrcozelen: '#8DF58C',
+        mytemnozelen: '#9AD479',
+        myheitralnu: '#D1EB91',
+        mytemnogoltui: '#D4D279',
+        myyrcogoltui: '#F5E98C',
+        veryDarkBlue: 'hsl(228, 12%, 13%)',
+        myslate: '#d4e5fc',
+        myblue: '#6da2f8' ,
+      },
+      fontFamily: {
+        sans: ['Carlito'],
+      },
+    },
+  },
+  plugins: [],
+}
